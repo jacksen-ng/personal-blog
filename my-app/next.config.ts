c1cba036ba52dc62d23fd2cb41dac673eb57a441
jacksen-ng <ngjacksen04@gmail.com>
2025-04-12 10:46:56 +0900

    feat: update domain configuration

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['jacksen-blog.com'],
+    domains: ['jacksen-blog.com', 'www.jacksen-blog.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.jacksen-blog.com',
+        permanent: true,
+      },
+    ];
   },
 };
 
